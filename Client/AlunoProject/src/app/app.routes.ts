1b966948098ee957cde883984010741a0eeff58d
Rafael Kowalski <rafaelmk06@gmail.com>
2024-02-27 19:22:06 -0300

    Alterando estrutura e iniciando projeto front end

---
+++ Client/AlunoProject/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { ProfessoresComponent } from './professores/professores.component';
+
+export const routes: Routes = [
+  {
+    path: 'professores',
+    component: ProfessoresComponent
+  }
+];
